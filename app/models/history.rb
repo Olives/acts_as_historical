f1c881d4566ac70eeb8b987cfc5968d81c72ce6b
Kyle d'Oliveira <kdoliveira@bravenet.com>
2011-10-03 15:14:30 -0700

    ar association key name gets added to habtm records sometimes. should be filtered out

--- app/models/history.rb
+++ app/models/history.rb
@@ -65,7 +65,7 @@ class History < ActiveRecord::Base
     def remove_unwanted_fields(options, changes)
       changes.symbolize_keys!
 
-      [(options[:except]||[]), [:updated_at, :created_at]].flatten.each do |unwanted_field|
+      [(options[:except]||[]), [:ar_association_key_name, :updated_at, :created_at]].flatten.each do |unwanted_field|
         changes.delete(unwanted_field.intern)
       end
       if options[:only]
